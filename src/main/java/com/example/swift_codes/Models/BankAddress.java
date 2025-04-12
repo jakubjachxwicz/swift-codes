f084dff5046d479706a32375a62f1f789777d328
Jakub Jachowicz <kubajachowicz39@gmail.com>
2025-04-12 19:21:02 +0200

    database schema configuration

---
+++ src/main/java/com/example/swift_codes/Models/BankAddress.java
@@ -0,0 +1,41 @@
+package com.example.swift_codes.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class BankAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long addressID;
+
+    @Column(nullable = true)
+    private String address;
+
+    @Column(nullable = false)
+    private String townName;
+
+
+    public long getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(long addressID) {
+        this.addressID = addressID;
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public void setTownName(String townName) {
+        this.townName = townName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/example/swift_codes/Models/Country.java
@@ -0,0 +1,53 @@
+package com.example.swift_codes.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long countryID;
+
+    @Column(nullable = false)
+    private String countryCode;
+
+    @Column(nullable = false)
+    private String countryName;
+
+    @Column(nullable = false)
+    private String timeZone;
+
+
+    public long getCountryID() {
+        return countryID;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+
+    public void setCountryID(long countryID) {
+        this.countryID = countryID;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+}
