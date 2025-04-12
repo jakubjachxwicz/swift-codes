2a49b8112ea9992ddccb8591b1df72024778bfff
Jakub Jachowicz <kubajachowicz39@gmail.com>
2025-04-12 16:30:10 +0200

    project init

---
+++ src/main/java/com/example/swift_codes/SwiftCodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.swift_codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftCodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swift_codes/SwiftCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swift_codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
