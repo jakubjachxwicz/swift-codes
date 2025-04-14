e306c22c7ef276ea54893f92c39b1e62380923c9
Jakub Jachowicz <kubajachowicz39@gmail.com>
2025-04-14 20:03:08 +0200

    first endpoint

--- src/main/java/com/example/swift_codes/Controllers/ApiController.java
+++ src/main/java/com/example/swift_codes/Controllers/ApiController.java
@@ -1,42 +1,34 @@
 package com.example.swift_codes.Controllers;
 
 import com.example.swift_codes.Models.BankAddress;
-import com.example.swift_codes.Models.BankName;
-import com.example.swift_codes.Models.Country;
 import com.example.swift_codes.Models.SwiftCode;
-import com.example.swift_codes.Repos.IBankAddressRepo;
-import com.example.swift_codes.Repos.IBankNameRepo;
-import com.example.swift_codes.Repos.ICountryRepo;
-import com.example.swift_codes.Repos.ISwiftCodeRepo;
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvValidationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.swift_codes.Repos.*;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.List;
 
 @RestController
-public class ApiController {
-
+public class ApiController
+{
     private final ICountryRepo countryRepo;
     private final ISwiftCodeRepo swiftCodeRepo;
     private final IBankNameRepo bankNameRepo;
     private final IBankAddressRepo bankAddressRepo;
 
     public ApiController(ICountryRepo countryRepo,
-                         ISwiftCodeRepo swiftCodeRepo,
-                         IBankNameRepo bankNameRepo,
-                         IBankAddressRepo bankAddressRepo)
+                              ISwiftCodeRepo swiftCodeRepo,
+                              IBankNameRepo bankNameRepo,
+                              IBankAddressRepo bankAddressRepo)
     {
         this.countryRepo = countryRepo;
         this.swiftCodeRepo = swiftCodeRepo;
@@ -45,85 +37,34 @@ public class ApiController {
     }
 
 
-    @PostMapping(value = "/parse-data")
-    public String parseData(@RequestParam String filename)
+    @GetMapping(value = "/v1/swift-codes/{swift-code}")
+    public ResponseEntity<String> getSwiftCodeDetails(@PathVariable("swift-code") String code)
     {
-        CSVReader csvReader;
-        String[] record;
+        SwiftCode swiftCodeRecord = swiftCodeRepo.findBySwiftCode(code)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "SWIFT code not found"));
 
-        try
+        ObjectNode response = JsonNodeFactory.instance.objectNode()
+                .put("address", swiftCodeRecord.getBankAddress().getAddress())
+                .put("bankName", swiftCodeRecord.getBankName().getBankName())
+                .put("countryISO2", swiftCodeRecord.getCountry().getCountryCode())
+                .put("countryName", swiftCodeRecord.getCountry().getCountryName())
+                .put("isHeadquarter", swiftCodeRecord.getIsHeadquarters())
+                .put("swiftCode", swiftCodeRecord.getSwiftCode());
+
+        if (swiftCodeRecord.getIsHeadquarters())
         {
-            ClassPathResource resource = new ClassPathResource(filename);
+            ArrayNode branchesArray = response.putArray("branches");
 
-            csvReader = new CSVReaderBuilder(new InputStreamReader(resource.getInputStream()))
-                    .withCSVParser(new CSVParserBuilder().build())
-                    .withSkipLines(1)
-                    .build();
-
-            while ((record = csvReader.readNext()) != null)
-            {
-                String countryCode = record[0];
-                String swiftCode = record[1];
-                String codeType = record[2];
-                String name = record[3];
-                String address = record[4];
-                String townName = record[5];
-                String countryName = record[6];
-                String timeZone = record[7];
-
-                Country country = getOrCreateCountry(countryCode, countryName, timeZone);
-                BankName bankName = getOrCreateBankName(name);
-                BankAddress bankAddress = getOrCreateAddress(address, townName);
-
-                SwiftCode swiftCodeRecord = new SwiftCode();
-                swiftCodeRecord.setSwiftCode(swiftCode);
-                swiftCodeRecord.setCodeType(codeType);
-                swiftCodeRecord.setCountry(country);
-                swiftCodeRecord.setBankName(bankName);
-                swiftCodeRecord.setBankAddress(bankAddress);
-                if (swiftCode.endsWith("XXX"))
-                    swiftCodeRecord.setHeadquarters(true);
-                else
-                    swiftCodeRecord.setHeadquarters(false);
-                swiftCodeRepo.save(swiftCodeRecord);
-            }
-        } catch (CsvValidationException | IOException e) {
-            throw new RuntimeException(e);
+            List<SwiftCode> bankBranches = swiftCodeRepo.getBankBranches(code);
+            bankBranches.forEach(b -> branchesArray.addObject()
+                    .put("address", b.getBankAddress().getAddress())
+                    .put("bankName", b.getBankName().getBankName())
+                    .put("countryISO2", b.getCountry().getCountryCode())
+                    .put("isHeadquarter", b.getIsHeadquarters())
+                    .put("swiftCode", b.getSwiftCode()
+            ));
         }
 
-        return filename;
-    }
-
-    private Country getOrCreateCountry(String code, String name, String timeZone)
-    {
-        return countryRepo.findByCountryCode(code).orElseGet(() -> {
-            Country country = new Country();
-            country.setCountryCode(code);
-            country.setCountryName(name);
-            country.setTimeZone(timeZone);
-
-            return countryRepo.save(country);
-        });
-    }
-
-    private BankName getOrCreateBankName(String name)
-    {
-        return bankNameRepo.findByBankName(name).orElseGet(() -> {
-            BankName bankName = new BankName();
-            bankName.setBankName(name);
-
-            return bankNameRepo.save(bankName);
-        });
-    }
-
-    private BankAddress getOrCreateAddress(String address, String townName)
-    {
-        return bankAddressRepo.findByAddress(address).orElseGet(() -> {
-           BankAddress bankAddress = new BankAddress();
-           bankAddress.setAddress(address);
-           bankAddress.setTownName(townName);
-
-           return bankAddressRepo.save(bankAddress);
-        });
+        return ResponseEntity.ok(response.toString());
     }
 }
--- src/main/java/com/example/swift_codes/Repos/ISwiftCodeRepo.java
+++ src/main/java/com/example/swift_codes/Repos/ISwiftCodeRepo.java
@@ -2,6 +2,18 @@ package com.example.swift_codes.Repos;
 
 import com.example.swift_codes.Models.SwiftCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ISwiftCodeRepo extends JpaRepository<SwiftCode, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface ISwiftCodeRepo extends JpaRepository<SwiftCode, Long>
+{
+    Optional<SwiftCode> findBySwiftCode(String swiftCode);
+
+    @Query("SELECT sc FROM SwiftCode sc " +
+            "WHERE SUBSTRING(sc.swiftCode, 1, 8) = SUBSTRING(:code, 1, 8) " +
+            "AND sc.swiftCode <> :code")
+    List<SwiftCode> getBankBranches(@Param("code") String code);
 }
